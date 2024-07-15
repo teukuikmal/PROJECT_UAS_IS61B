588a32107916cce33342d3968aefa8f57ab57976
Teuku Ikmal <98931566+teukuikmal@users.noreply.github.com>
2024-07-15 13:03:44 +0700

    Update Route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MahasiswaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::controller(MahasiswaController::class)->prefix('mahasiswa')->group(function() {
+    Route::get('', 'index')->name('mahasiswa');
+    Route::get('insert', 'add')->name('mahasiswa.insert');
+    Route::post('insert', 'insert')->name('mahasiswa.add.insert');
+    Route::get('{id}/edit', 'edit')->name('mahasiswa.edit');
+    Route::put('{id}/edit', 'update')->name('mahasiswa.update');
+    Route::delete('{id}', 'destroy')->name('mahasiswa.delete');
 });
